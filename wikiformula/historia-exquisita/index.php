c0ddb519a9741884f16c3dfe3ad5265b61f70eb7
Patricia Jarpa <62717165+pjarpa@users.noreply.github.com>
2020-08-08 21:00:21 +0200

    Add files via upload

---
+++ wikiformula/historia-exquisita/index.php
@@ -0,0 +1,235 @@
+<?php ob_start(); ?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+	<meta charset="utf-8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="index, follow" />
+		<meta name="generator" content="RapidWeaver" />
+		
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="historia">
+	<meta name="twitter:image" content="https://pjarpa.github.io/wikiformula/resources/imagen_cerveza.jpg">
+	<meta name="twitter:url" content="https://pjarpa.github.io/wikiformula/historia-exquisita/index.php">
+	<meta property="og:type" content="website">
+	<meta property="og:site_name" content="Wiki Fórmula <em>Lugar de encuentro para fanáticos</em>">
+	<meta property="og:title" content="historia">
+	<meta property="og:image" content="https://pjarpa.github.io/wikiformula/resources/imagen_cerveza.jpg">
+	<meta property="og:url" content="https://pjarpa.github.io/wikiformula/historia-exquisita/index.php"> 
+
+	<!-- Meta tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+	<title>historia</title>
+	<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/Artful/consolidated.css?rwcache=618605811" />
+		
+	    		<link rel='stylesheet' type='text/css' media='all' href='../rw_common/plugins/stacks/stacks.css?rwcache=618605811' />
+		<link rel='stylesheet' type='text/css' media='all' href='files/stacks_page_page0.css?rwcache=618605811' />
+        <script type='text/javascript' charset='utf-8' src='../rw_common/plugins/stacks/jquery-2.2.4.min.js?rwcache=618605811'></script>
+        
+        <link rel="stylesheet" href="../rw_common/plugins/stacks/font-awesome.min.css?rwcache=618605811">
+        
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+		<script type='text/javascript' charset='utf-8' src='files/stacks_page_page0.js?rwcache=618605811'></script>
+        <meta name="formatter" content="Stacks v4.0.3 (4795)" >
+		<meta class="stacks4 stack version" id="com.joeworkman.stacks.easycms.admin.styles" name="Admin Core" content="1.3.1">
+		<meta class="stacks4 stack version" id="com.joeworkman.stacks.easycms.admin.text" name="Text" content="1.3.1">
+		<meta class="stacks4 stack version" id="com.stacks4stacks.stacks.spacer" name="Spacer" content="1.4.2">
+		
+
+</head>
+
+<!-- This page was created with RapidWeaver from Realmac Software. http://www.realmacsoftware.com -->
+
+<body>
+	<nav class="navbar navbar-expand-lg">
+		<div class="container">
+            <a class="navbar-brand" href="https://pjarpa.github.io/wikiformula/">
+                <div class="logo"><img src="../rw_common/images/logo-wiki-big.png" width="2000" height="551" alt="Wiki F&oacute;rmula <em>Lugar de encuentro para fan&aacute;ticos</em>"/></div>
+                <span class="navbar-title">Wiki F&oacute;rmula <em>Lugar de encuentro para fan&aacute;ticos</em></span>
+            </a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+			 aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../musica/" rel="" class="nav-link">Música</a></li><li class="nav-item active"><a href="./" rel="" class="nav-link">Historia Exquisita</a></li><li class="nav-item"><a href="../blog/" rel="" class="nav-link">Recetas</a></li></ul>
+			</div>
+		</div>
+	</nav>
+	<div class="hero pb-5">
+		<div class="container">
+			<div class="row align-items-center justify-content-center">
+				<div class="col-sm-6 align-self-center">
+					<h1 class="hero-title mb-4">
+						Wiki F&oacute;rmula <em>Lugar de encuentro para fan&aacute;ticos</em>
+					</h1>
+					<p class="lead hero-slogan">
+						Aqu&iacute; encontrar&aacute;s interesantes datos de la historia tras la f&oacute;rmula secreta y un lugar donde compartir experiencias
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<img src="../rw_common/images/imagen_cerveza.jpg" width="100" alt="" class="banner_image">
+				</div>
+			</div>
+		</div>
+    </div>
+
+    <hr class="my-5">
+
+	<div class="container pt-5">
+        <div class="row intro justify-content-between">
+            <div class="col-sm-6">
+                
+<div id='stacks_out_1_page0' class='stacks_top'><div id='stacks_in_1_page0' class=''><div id='stacks_out_2_page0' class='stacks_out'><div id='stacks_in_2_page0' class='stacks_in text_stack'>"F&oacute;rmula Secreta" contin&uacute;a. Te invitamos a jugar y crear en torno a la historia, escribiendo a continuaci&oacute;n en una frase lo qu&eacute; crees que la detective obtuvo como recompensa y que pas&oacute; despu&eacute;s de eso&hellip;</div></div>
+
+<?php
+if (!isset($cms_foundation_checker)) {
+	$cms_foundation_checker = true;
+	$page_check = ob_get_clean();
+	ob_start();
+	echo $page_check;
+	if (strpos($page_check,"foundation-version") === false) {
+		echo '<link rel="stylesheet" type="text/css" media="all" href="../rw_common/plugins/stacks/total-cms/total-cms.css"/>'.
+			 '<script type="text/javascript" charset="utf-8" src="../rw_common/plugins/stacks/total-cms/total-cms.js"></script>';
+	}
+}
+?>
+
+
+
+
+
+
+
+
+
+
+<div id="cms-alertbox-success" class="cms-alertbox"><i class="fa fa-check fa-3x"></i></div>
+<div id="cms-alertbox-error" class="cms-alertbox"><i class="fa fa-times fa-3x"></i></div>
+
+<!--
+	Image Preview Template
+-->
+<script type="text/template" id="image-preview-template">
+<div class="dz-preview dz-file-preview">
+	<div class="actionbar">
+		<a class="imagebar-tag left"   href="javascript:void(0)" title="Image Description"><i class="fa fa-tag"></i></a>
+		<a class="imagebar-image left" href="javascript:void(0)" title="Copy Image Paths"><i class="fa fa-image"></i></a>
+		<a class="imagebar-featured left"  href="javascript:void(0)" title="Featured Image"><i class="fa fa-star-o"></i><i class="fa fa-star"></i></a>
+		<a class="imagebar-trash right" href="javascript:void(0)" title="Delete Image"><i class="fa fa-trash-o"></i></a>
+		<i class="fa fa-fw fa-bars imagebar-move"></i>
+	</div>
+	<img oncontextmenu="return false;" draggable="false" data-dz-thumbnail />
+	<div class="dz-progress">
+		<span class="dz-upload" data-dz-uploadprogress></span>
+		<span class="dz-upload-progress-label" data-dz-uploadprogress>0%</span>
+	</div>
+	<i class="fa fa-check-circle"></i>
+	<i class="fa fa-times-circle has-tip" data-tooltip title="Error"></i>
+</div>
+</script>
+<!--
+	Copy Image Path Reveal Template
+-->
+<script type="text/template" id="imagebar-image-template">
+<div id="imagebar-image" class="reveal-modal" data-reveal>
+	<h4>Copy Image Paths</h4>
+	<hr/>
+	<label>Full Image</label>
+	<input type="text" name="image" readonly/>
+	<label>Thumbnail</label>
+	<input type="text" name="thumb" readonly/>
+	<label>Square Thumb</label>
+	<input type="text" name="square" readonly/>
+
+	<img src="" alt=""/>
+	<a class="close-reveal-modal">&#215;</a>
+</div>
+</script>
+<!--
+	Alt Tag Reveal Template
+-->
+<script type="text/template" id="altbox-template">
+<div id="altbox" class="reveal-modal" data-reveal>
+	<h4>Image Description</h4>
+	<hr/>
+
+	<form class="total-form alt-form" autocomplete="off" method="post" action="../rw_common/plugins/stacks/total-cms/totalapi.php" data-baseurl="https://pjarpa.github.io/wikiformula/">
+
+		<fieldset class="text-box textarea">
+			<textarea class="track-selection" name="alt" spellcheck="true" rows="2" placeholder="Enter Image Description"></textarea>
+		</fieldset>
+
+		<input type="hidden" name="_METHOD" value="PUT"/>
+		<input type="hidden" name="type"  value="image"/>
+		<input type="hidden" name="filename" value="false"/>
+		<input type="hidden" name="timestamp" />
+		<input type="hidden" name="ext"   value="jpg"/>
+		<input type="hidden" name="slug"  value=""/>
+		<input type="hidden" name="permalink" value=""/>
+
+		<button class="button radius" type="submit" value="Save">Save</button>
+	</form>
+
+	<a class="close-reveal-modal">&#215;</a>
+</div>
+</script>
+
+
+
+
+<div id='stacks_out_35_page0' class='stacks_out'><div id='stacks_in_35_page0' class='stacks_in com_stacks4stacks_stacks_spacer_stack'><!-- Spacer stack by Will Woodgate. Visit http://stacks4stacks.com for more information -->
+<div id="spacerStackstacks_in_35_page0" class="spacerStack"></div></div></div><div id='stacks_out_34_page0' class='stacks_out'><div id='stacks_in_34_page0' class='stacks_in com_joeworkman_stacks_easycms_admin_text_stack'><!-- TotalCMS Text Form -->
+<form autocomplete="off" class="total-form text-form" method="post" action="../rw_common/plugins/stacks/total-cms/totalapi.php">
+
+	<fieldset class="text-box textarea hipwig">
+
+		
+			<textarea class="hipwig" spellcheck="true" rows="3" data-height="200" placeholder="Insert your text here" name="text" data-maxcount="-1"></textarea>
+		
+
+	</fieldset>
+
+	<input type="hidden" name="slug" value="totalcms" />
+	<input type="hidden" name="type" value="text" />
+
+	
+
+</form>
+
+
+
+
+</div></div></div></div>
+
+            </div>
+            <div class="sidebar col-sm-6 order-md-8">
+                <h2 class="sidebar-title">
+                    
+                </h2>
+
+                 
+            </div>
+        </div>
+	</div>
+
+	<div class="footer">
+		<div class="container">
+			<div class="row justify-content-start">
+				<div class="col">
+					<div class="footer-content">
+						&copy; 2020 F&oacute;rmula Secreta
+					</div>
+
+					<ul class="navbar-nav ml-auto"><li class="nav-item"><a href="../" rel="" class="nav-link">Home</a></li><li class="nav-item"><a href="../musica/" rel="" class="nav-link">Música</a></li><li class="nav-item active"><a href="./" rel="" class="nav-link">Historia Exquisita</a></li><li class="nav-item"><a href="../blog/" rel="" class="nav-link">Recetas</a></li></ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="../rw_common/themes/Artful/js/main.js?rwcache=618605811"></script>
+</body>
+
+</html>
